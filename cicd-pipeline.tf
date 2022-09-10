01cb7718badd203f845e767e4c9b4387482125b2
Khanyile <skhuslk@amazon.com>
2022-09-10 14:12:58 +0200

    all changes

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -56,7 +56,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
 
     artifact_store {
         type="S3"
-        location = aws_s3_bucket.us-east-1-artifact-output-bucket.bucket
+        location = aws_s3_bucket.codepipeline_artifacts.id
     }
 
     stage {
